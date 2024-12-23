b27942151244f0fa9373c4985fa4e4bd865b25a6
Ruben <RubenTbaer@gmail.com>
2024-12-23 13:50:23 +0100

    pomodoro

---
+++ DOM/03-selectores-un-elemento/script.js
@@ -0,0 +1,6 @@
+// Selector de Id
+const tituloPrincipal = document.getElementById("titulo-principal");
+console.log(tituloPrincipal.innerText);
+
+//con query selector
+const tituloPrincipalquery = document.querySelector("#titulo-principal");
---
+++ DOM/reloj/script.js
@@ -0,0 +1,17 @@
+//optener todos los enlances
+out = document.links;
+
+const enlaces = document.links
+for (let link of enlaces) {
+    console.log(link.href);
+    
+}
+//
+out document.images
+const imagenes = document.images
+for(let imagen of imagenes){
+    console.log(imagen.src);
+    
+}
+//
+const imgArray
