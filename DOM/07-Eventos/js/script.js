83a4c87750f8fe64089f75904750038b1e370cda
Ruben <RubenTbaer@gmail.com>
2025-01-13 10:42:01 +0100

    javita

--- DOM/07-Eventos/js/script.js
+++ DOM/07-Eventos/js/script.js
@@ -14,6 +14,44 @@ function getKeyCodeInnerHTML(e) {
 function getKeyCodeCreateNodes(e) {
   //TODO obtener la mima funcionalidad que la funcion anterior
   //pero usando createElement, createTextNode y appendChild
+  //Sin innerHTML
+  const insertDiv = document.querySelector("#insert");
+  insertDiv.textContent = "";
+
+  const keyCodeArr = [
+    { name: "e.key", evento: e.key },
+    { name: "e.keyCode", evento: e.keyCode },
+    { name: "e.code", evento: e.code },
+  ];
+
+  keyCodeArr.forEach((keyInfo) => {
+    const div = createDiv(keyInfo.name, keyInfo.evento);
+    insertDiv.appendChild(div);
+  });
+  //   const keyCodes = {
+  //     "e.key": e.key,
+  //     "e.keyCode": e.keyCode,
+  //     "e.code": e.code,
+  //   };
+
+  //   for (let key in keyCodes) {
+  //     const div = createDiv(key, keyCodes["key"]);
+  //     insertDiv.appendChild(div);
+  //   }
 }
 // window.addEventListener("keyup", getKeyCodeInnerHTML);
 window.addEventListener("keyup", getKeyCodeCreateNodes);
+
+function createDiv(_keyText, _valueText) {
+  const div = document.createElement("div");
+  div.classList.add("key");
+
+  const small = document.createElement("small");
+  const keyText = document.createTextNode(_keyText);
+  small.appendChild(keyText);
+
+  const valueText = document.createTextNode(_valueText);
+  div.appendChild(valueText);
+  div.appendChild(small);
+  return div;
+}
